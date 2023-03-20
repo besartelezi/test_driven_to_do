572255537cd2422e830fdb4f1810f13e7c9ea075
besartelezi <besart.elezi@hotmail.com>
2023-03-20 14:11:24 +0100

    added user stories and expanded test

--- functional_tests.py
+++ functional_tests.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 import unittest
 
+# Tests are organized into classes, which inherit from unittest.TestCase
 class NewVisitorTest(unittest.TestCase):  
 
+    #* setUp and tearDown = SPECIAL METHODS that get run before and after each test
     def setUp(self):  
         self.browser = webdriver.Firefox()
 
     def tearDown(self):  
         self.browser.quit()
 
+    #* any method whose name starts with test is a test method, and it will be run by the test runner
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
+
+        #* assertin = special test assertions, look at documentation to check them all
         self.assertIn('To-Do', self.browser.title)  
+        #* self.fail = fails no matter what
         self.fail('Finish the test!')  
 
         # She is invited to enter a to-do item straight away
@@ -39,5 +45,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
+#! checks if it's been executed from command line, rather than imported from another script
 if __name__ == '__main__':  
+    #* calling unittest main, launches unittest test runner which automatically finds test classes and methods in the file and runs them
     unittest.main()
