9e9c5374f5e840e410d39e6e5daff399438cc234
besartelezi <besart.elezi@hotmail.com>
2023-03-21 11:30:03 +0100

    functional test now checks wwe can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 # Tests are organized into classes, which inherit from unittest.TestCase
@@ -21,19 +23,41 @@ class NewVisitorTest(unittest.TestCase):
 
         #* assertin = special test assertions, look at documentation to check them all
         self.assertIn('To-Do', self.browser.title)  
-        #* self.fail = fails no matter what
-        self.fail('Finish the test!')  
+        # #* self.fail = fails no matter what
+        # Best to place this test at the end of all written tests, and remove it once all tests have been added
+        # self.fail('Finish the test!')  
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        #* .send_keys = selenium's way of inputting keys
+        inputbox.send_keys('Buy peacock feathers')  
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        #* Keys class lets us input special keys, like ENTER
+        inputbox.send_keys(Keys.ENTER) 
+        #* The time.sleep is there to make sure the browser has finished loading before we make any assertions about the new page
+        time.sleep(1)
 
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
