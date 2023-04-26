ccd4caba7afa197229c3cce92c4c851159fad5ce
besartelezi <besart.elezi@hotmail.com>
2023-04-26 17:45:35 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -123,7 +123,29 @@ class NewVisitorTest(LiveServerTestCase):
 
         # satisfied, they both go back to sleep 
 
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
 
+        # She notices that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # she starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 #! checks if it's been executed from command line, rather than imported from another script
 if __name__ == '__main__':  
