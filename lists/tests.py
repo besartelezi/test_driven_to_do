1a818dcdc6e5d45b4934d04815c2b1d141dee1d2
besartelezi <besart.elezi@hotmail.com>
2023-04-26 11:36:36 +0200

    changed files, did some mogrations, fixed bug in mogrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -8,17 +8,33 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        list_ = List()
+        list_.save()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+class ListAndItemModelTest(TestCase):
 
     #* creating new objects and storing then in database
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -26,25 +42,17 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        # BTS, this also checks for the id number of the list property
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        # BTS, this also checks for the id number of the list property
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_all_items(self):
-        # setup
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        # exercise
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-        # assert
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
 
 class NewListTest(TestCase):
 
