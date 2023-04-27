77393d48dd65ac935cb7537dadf8d396904254fe
besartelezi <besart.elezi@hotmail.com>
2023-04-27 10:45:05 +0200

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +8,7 @@ import unittest
 MAX_WAIT = 10
 
 # Tests are organized into classes, which inherit from unittest.TestCase
-class NewVisitorTest(LiveServerTestCase):  
+class NewVisitorTest(StaticLiveServerTestCase):  
 
     #* setUp and tearDown = SPECIAL METHODS that get run before and after each test
     def setUp(self):  
