f01e2b8ffa04dfc26ed2df2e475a7fcc1fb840b7
besartelezi <besart.elezi@hotmail.com>
2023-05-08 14:56:08 +0200

    replaced run commands with sudo

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,16 +1,14 @@
 import random
 from fabric.contrib.files import append, exists
-from fabric.api import cd, env, local, run
+from fabric.api import cd, env, local, sudo
 import os, getpass
 
 REPO_URL = 'https://github.com/besartelezi/test_driven_to_do'
 
 def deploy():
     print("checking if folder already exists...")
-    site_folder = f'~/sites/{env.host}'
-    # if not exists(site_folder):
-    #     print("creating folder...")+
-    #     os.makedirs(f'{site_folder}')
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    sudo(f'mkdir -p {site_folder}')
     print(site_folder + ' : is path currently in use')
     with cd(site_folder):
         print("getting latest source")
@@ -26,22 +24,22 @@ def _get_latest_source():
     print("function has been successfully called")
     if exists('.git'):
         print("git fetching")
-        run('git fetch')
+        sudo('git fetch')
     else:
         print("git cloning")
-        run(f'git clone {REPO_URL} .')
+        sudo(f'git clone {REPO_URL} .')
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
+    sudo(f'git reset --hard {current_commit}')
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
-        run(f'python3.7 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+        sudo(f'python3.7 -m venv virtualenv')
+    sudo('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')
+    current_contents = sudo('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(  
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
@@ -49,7 +47,7 @@ def _create_or_update_dotenv():
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')  
+    sudo('./virtualenv/bin/python manage.py collectstatic --noinput')  
 
 def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')  
+    sudo('./virtualenv/bin/python manage.py migrate --noinput')  
