2672c789328854f5e35de4a60394926dabebe7c4
besartelezi <besart.elezi@hotmail.com>
2023-05-09 10:39:47 +0200

    Moved fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,40 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import unittest
-import os
 
-MAX_WAIT = 10
 
-# Tests are organized into classes, which inherit from unittest.TestCase
-class NewVisitorTest(StaticLiveServerTestCase):  
-
-    #* setUp and tearDown = SPECIAL METHODS that get run before and after each test
-    def setUp(self):  
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):  
-        self.browser.quit()
-
-    #* made a method that gets called in the test
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
 
     #* any method whose name starts with test is a test method, and it will be run by the test runner
     def test_can_start_a_list_for_one_user(self):  
@@ -126,32 +95,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # satisfied, they both go back to sleep 
-
-    def test_layout_and_styling(self):
-        # Edith goes to the homepage
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
-
-        # She notices that the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # she starts a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-#! checks if it's been executed from command line, rather than imported from another script
-if __name__ == '__main__':  
-    #* calling unittest main, launches unittest test runner which automatically finds test classes and methods in the file and runs them
-    unittest.main()
