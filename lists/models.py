41cd1ec6c6dc436a2a75404f7b2834450ea5d0c1
besartelezi <besart.elezi@hotmail.com>
2023-05-17 10:38:34 +0200

    Implement item validation at model layer

--- lists/models.py
+++ lists/models.py
@@ -10,3 +10,11 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ('id',)
+        # Sets of field names that, taken together, must be unique
+        unique_together = ('list', 'text')
